16f0cf793944b46aca8cebf9cb5017e50b50a29b
Lucas Lima <lucaspecheche@gmail.com>
2018-10-21 22:35:38 -0400

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/', 'PostController@index')->name('home');
+
+Route::resource('users', 'UserController');
+
+Route::resource('roles', 'RoleController');
+
+Route::resource('permissions', 'PermissionController');
+
+Route::resource('posts', 'PostController');
+
+/*
+Route::group( ['middleware' => ['auth']], function() {
+    Route::resource('users', 'UserController');
+    Route::resource('roles', 'RoleController');
+    Route::resource('posts', 'PostController');
+});
+*/
